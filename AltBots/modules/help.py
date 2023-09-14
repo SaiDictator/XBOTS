4b6d8ead723571ce313578e45241a7208b81ad49
SaiDictator <141777716+SaiDictator@users.noreply.github.com>
2023-09-14 22:43:51 +0530

    Update help.py

--- AltBots/modules/help.py
+++ AltBots/modules/help.py
@@ -14,8 +14,8 @@ HELP_BUTTON = [
       Button.inline("• ᴇxᴛʀᴀ •", data="extra")
     ],
     [
-      Button.url("• ᴄʜᴀɴɴᴇʟ •", "https://t.me/DAEDX_HELP"),
-      Button.url("• sᴜᴘᴘᴏʀᴛ •", "https://t.me/DAEDX_HELP")
+      Button.url("• ᴄʜᴀɴɴᴇʟ •", "https://t.me/DAEDX_HELP/4"),
+      Button.url("• sᴜᴘᴘᴏʀᴛ •", "https://t.me/DAEDX_HELP/4")
     ]
   ]
 
